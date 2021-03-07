0e3fd95c01073f487deccb558c913076998c098d
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2021-03-08 02:24:30 +0800

    OOP-ed Code + doxygen commented GUIWidget.py

--- GUI.py
+++ GUI.py
@@ -1,42 +1,12 @@
-import sys
-#Requires Installation of PyQt5
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import *
 from GUIWidgets import *
-from newWindow import Ui_newWindow
 
-class GUIWindow:
-    def __init__(self, Window):
-        self.setSearchButton(Window)
-    
-    def setSearchButton(self, Window):
-        self.SearchButton = QPushButton(Window)
-        self.SearchButton.setGeometry(QtCore.QRect(370, 280, 81, 41))
-        self.SearchButton.setText("Search")
-        self.SearchButton.clicked.connect(self.searchClicked)
-    
-    def searchClicked(self):
-        self.yturl = self.urlbox.text()
-        self.tuid = self.uidbox.text()
-        if (self.yturl == "" or self.tuid == ""):
-            showerror = self.errorMsgBox()
-        else:
-            self.newwin = QMainWindow()
-            self.newui = Ui_newWindow()
-            self.newui.setupUi(self.newwin)
-            self.newui.label_2 .setText(self.yturl)
-            self.newui.label_2.adjustSize()
-            self.newui.label_3.setText(self.tuid)
-            self.newui.label_3.adjustSize()
-            self.newwin.show()
-
-    def errorMsgBox(self):
-        msgBox = QMessageBox()
-        msgBox.setWindowTitle("Error!")
-        msgBox.setWindowIcon(QtGui.QIcon("CookieIcon.png"))
-        msgBox.setIcon(QMessageBox.Critical)
-        msgBox.setText("Error! Missing Input!")
-        error = msgBox.exec_()
+def searchClicked():
+    YTURL = URLBox.textbox.text()
+    TURL = TURLBox.textbox.text()
+    if (YTURL == "" or TURL == ""):
+        print("Error!")
+    else:
+        print(YTURL,TURL)
 
 App = StartApp()
 MainWindow = NewWindow("Cookie Crawler", 800, 600)
@@ -49,10 +19,11 @@ URLLabel.setText("Enter URL:")
 UIDLabel = NewLabel(MainWindow.QWin,260, 200, 61, 31)
 UIDLabel.setText("Enter UID:")
 URLBox = NewTextBox(MainWindow.QWin, 120, 160, 591, 31)
-URLBox.setPlaceHolderText("Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>")
-TUIDBox = NewTextBox(MainWindow.QWin, 120, 200, 591, 31)
-TUIDBox.setPlaceHolderText("Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>")
-ui = GUIWindow(MainWindow.QWin)
+URLBox.setText("Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>")
+TURLBox = NewTextBox(MainWindow.QWin, 120, 200, 591, 31)
+TURLBox.setText("Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>")
+SearchPushButton = NewPushButton(MainWindow.QWin, 370, 250, 81, 41, searchClicked)
+SearchPushButton.setText("Crawl Link!")
 MainWindow.QWin.show()
 sys.exit(App.QApp.exec_())
 
--- GUIWidgets.py
+++ GUIWidgets.py
@@ -1,44 +1,89 @@
+## @file GUIWidgets.py
+#
+# @brief this file contains GUI Widget Classes
+#
+# @section libraries_main Libraries/Modules
+# - sys standard library (https://docs.python.org/3/library/sys.html)
+#   - access to sys.argv and sys.exit functions
+# - PyQt5 external library
+#   - access to PyQt5 GUI functions
+# - PyQt5.QtWidgets external library
+#   - access to PyQt5 UI Widgets
+
+#Imports
 import sys
-#Requires Installation of PyQt5
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
-from newWindow import Ui_newWindow
+
+## Documentation for GUIWidgets.py
+# Contains all UI Widget classes
+# We can reuse classes to make different widgets for diffferent purpose
 
 #Class to initialize a new instance of QApplication module which is required to run PyQt5
 class StartApp:
+    """! StartApp class
+    Defines the QApplication object which allows the creation of all QtWidgets
+    """
     def __init__(self):
         self.QApp = QApplication(sys.argv)
 
 #Class to create a new Window
 class NewWindow:
-    def __init__(self, name, lenx, leny):
+    """! NewWindow class
+    Defines the window object used to display widgets
+    """
+    def __init__(self, name, lenX, lenY):
+        """! NewWindow class initializer
+        @param name used to name the window title
+        @param lenX used to set the horizontal length of the window
+        @param lenY used to set the veritical height of the window
+        """
         #Initialize new instance of Window UI
         self.QWin = QMainWindow()
         #Set Window Title
         self.QWin.setWindowTitle(name)
         #Set Window Size
-        self.QWin.resize(lenx,leny)
+        self.QWin.resize(lenX,lenY)
 
     #Method to set Window icon image
     def setWindowIcon(self,image):
+        """! setWindowIcon method used to set image as Window Icon
+        @param image image name to be used as Window Icon
+        """
         self.QWin.setWindowIcon(QtGui.QIcon(image))
 
 #Class to create a new Label
 class NewLabel:
-    def __init__(self, Window, posx, posy, lenx, leny):
+    """! NewLabel class
+    Defines the label object used to display text label to guide users
+    """
+    def __init__(self, window, posX, posY, lenX, lenY):
+        """! NewLabel class initializer
+        @param window used to determine which window for the widget to appear on
+        @param posX used to set the X coordinate of where the label will appear
+        @param posY used to set the Y coordinate of where the label will appear
+        @param lenX used to set the horizontal length of the label
+        @param lenY used to set the vertical height of the label
+        """
         #Initialize new instance of Label UI
-        self.label = QLabel(Window)
-        #Set window x & y position and window size
-        self.label.setGeometry(QtCore.QRect(posx,posy,lenx,leny))
+        self.label = QLabel(window)
+        #Set Label x & y position and size
+        self.label.setGeometry(QtCore.QRect(posX, posY, lenX, lenY))
         #Set alignment of Label text to align center
         self.label.setAlignment(QtCore.Qt.AlignCenter)
 
     #Method to set Label Text
     def setText(self, text):
+        """! setText method 
+        @param text used to set the text to be displayed by the label
+        """
         self.label.setText(text)
     
     #Method to display image in Label
     def setImage(self, image):
+        """! setImage method
+        @param image used to determine what image to be displayed in label
+        """
         #Set display image in parameter in Label
         self.label.setPixmap(QtGui.QPixmap(image))
         #Enable image scaling to fit Label size
@@ -46,12 +91,53 @@ class NewLabel:
 
 #Class to create new TextBox
 class NewTextBox:
-    def __init__(self, Window, posx, posy, lenx, leny):
+    """! NewTextBox class
+    Defines the textbox object used to retrieve user input
+    """
+    def __init__(self, window, posX, posY, lenX, lenY):
+        """! NewTextBox class initializer
+        @param window used to determine which window for the widget to appear on
+        @param posX used to set the X coordinate of where the label will appear
+        @param posY used to set the Y coordinate of where the label will appear
+        @param lenX used to set the horizontal length of the label
+        @param lenY used to set the vertical height of the label
+        """
         #Initialize new instance of TextBox UI
-        self.textbox = QLineEdit(Window)
-        #Set window x & y position and window size
-        self.textbox.setGeometry(QtCore.QRect(posx, posy, lenx, leny))
+        self.textbox = QLineEdit(window)
+        #Set TextBox x & y position and size
+        self.textbox.setGeometry(QtCore.QRect(posX, posY, lenX, lenY))
 
     #Method to set palceholder text
-    def setPlaceHolderText(self, text):
+    def setText(self, text):
+        """! setText method
+        @param text used to set the text to be displayed by the label
+        """
         self.textbox.setPlaceholderText(text)
+
+#Class to create new PushButton
+class NewPushButton:
+    """! NewPushButton class
+    Defines the PushButton object to recieve button click input
+    """
+    def __init__(self, window, posX, posY, lenX, lenY, functionName):
+        """! NewPushButton class initializer
+        @param window used to determine which window for the widget to appear on
+        @param posX used to set the X coordinate of where the label will appear
+        @param posY used to set the Y coordinate of where the label will appear
+        @param lenX used to set the horizontal length of the label
+        @param lenY used to set the vertical height of the label
+        @param functionname used to determine which function to call when button is clicked
+        """
+        #Initialize new instance of PushButton UI
+        self.PushButton = QPushButton(window)
+        #Set PushButton x & y position and size
+        self.PushButton.setGeometry(QtCore.QRect(posX, posY, lenX, lenY))
+        #Calls function when PushButton is clicked
+        self.PushButton.clicked.connect(functionName)
+
+    #Set PushButton text
+    def setText(self, text):
+        """! setText method
+        @param text used to set the text to be displayed by the label
+        """
+        self.PushButton.setText(text)
