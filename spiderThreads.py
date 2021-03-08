2536153103dedf7402b2ad15c5b2625e33f7bfef
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-03-09 00:31:29 +0800

    spiders killed

--- spiderThreads.py
+++ spiderThreads.py
@@ -11,27 +11,29 @@ QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 RESULT_FILE = PROJECT_NAME + '/result.txt'
 queue = Queue() #create queue for spider threads
+alive = True
+
+class MyWorker(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.daemon = True
+        self.start()
+    def run(self):
+        global alive
+        while alive:
+            result_links = file_to_set(RESULT_FILE)
+            if len(result_links) >= NUMBER_OF_RESULTS:
+                print("enough results")
+                alive = False
+                break
+            url = queue.get()
+            Spider.crawl_page(threading.current_thread().name, url)
+            queue.task_done()
 
 # Create worker threads (will die when main exits)
 def create_workers():
-    for _ in range(NUMBER_OF_THREADS):
-        t = threading.Thread(target=work)
-        t.daemon = True
-        t.start()
-
-
-# Do the next job in the queue
-def work():
-    while True:
-        result_links = file_to_set(RESULT_FILE)
-        if len(result_links) >= NUMBER_OF_RESULTS:
-                print("enough results")
-                break
-
-        url = queue.get()
-        Spider.crawl_page(threading.current_thread().name, url)
-        queue.task_done()
-
+    for x in range(NUMBER_OF_THREADS):
+        MyWorker()
 
 # Each queued link is a new job
 def create_jobs():
@@ -42,9 +44,10 @@ def create_jobs():
 
 # Check if there are items in the queue, if so crawl them
 def crawl():
+    global alive
     queued_links = file_to_set(QUEUE_FILE)
 
-    if len(queued_links) > 0:
+    if alive and len(queued_links) > 0:
         print(str(len(queued_links)) + ' links in the queue')
         create_jobs()
 
@@ -61,5 +64,7 @@ def createSpider(filterList, topic=""):
     Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME, SUB_DOMAIN, filterList) #create first spider
     create_workers()
     crawl()
+    
 
 createSpider(['articles'], "covid test")
+print("test")
