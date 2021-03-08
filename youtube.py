a0aeae7d4e04a3fa3402a3f56b4148d7b0663301
Jokesama <77946630+Jokesama@users.noreply.github.com>
2021-03-09 00:33:10 +0800

    updated. needs refinement

--- youtube.py
+++ youtube.py
@@ -1,7 +1,9 @@
 from googleapiclient.discovery import build
+import re
 
 api_key = 'AIzaSyA_JNhdO1UNp8ww2CATP-BwuWbwqWC3Zxw'
 youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 class youtubeVid():
     # object variable
@@ -14,7 +16,7 @@ class youtubeVid():
     viewCount = 0
     likeCount = 0
     dislikeCount = 0
-    
+
     def getDict(self):
         videoDetail = {
             "title" : self.title,
@@ -34,7 +36,7 @@ def scrapData():
     countryCode = ['SG','MY','PH','KR','JP','TH','ID','TW']
     nextPageToken = None
     for code in countryCode:
-        for stuff in range(0,15):
+        for stuff in range(0,5):
             #request details
             request = youtube.videos().list(
                 part='snippet',
@@ -85,7 +87,99 @@ def vidInfo(video):
         video.dislikeCount = 0
     return video
 
-reply = scrapData()
-for i in range(0,len(reply)):
-    reply[i] = vidInfo(reply[i])
-    print(reply[i].getDict())
+# def playlistlength(playListID):
+#     request = youtube.playlistItems().list(
+#         part='contentDetails',
+#         playlistId=playListID
+#     )
+#     try:
+#         response = request.execute()
+#     except Exception as e:
+#         print(str(e))
+
+class channel():
+    country = ''
+    totalVid = 0
+    subCount = 0
+    totalview = 0
+    channelbd = ''
+    channelid = ''
+    description = ''
+    channel_name = ''
+    channel_thumbnail = ''
+    channelVids = []
+
+    def channelDetail(self):
+        channelDetail = {
+            "country" : self.country,
+            "totalVideos" : self.totalVid,
+            "subCount" : self.subCount,
+            "totalview" : self.totalview,
+            "channelbirthdate" : self.channelbd,
+            "channel id" : self.channelid,
+            "description" : self.description,
+            "channel name" : self.channel_name,
+            "channel thumbnail" : self.channel_thumbnail,
+            "channel Videos" : self.channelVids
+        }
+        return channelDetail
+
+    def getChannelDetail(chan):
+        channelList = []
+        request = youtube.channels().list(
+            part='snippet',
+            id=chan['channelID']
+        )
+        #execute the request
+        try:
+            response = request.execute()
+        except Exception as e:
+            print(str(e))
+        #appending the channel info
+        chn = channel()
+        chn.country = response["items"][0]["snippet"]["country"]
+        chn.channelbd = response["items"][0]["snippet"]["publishedAt"]
+        chn.channelid = response["items"][0]["id"]
+        chn.description = response["items"][0]["snippet"]["description"]
+        chn.channel_name = response["items"][0]["snippet"]["title"]
+        chn.channel_thumbnail = response["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        channelList.append(chn)
+        return channelList
+
+    def getChannelStat(channel):
+        statrequest = youtube.channels().list(
+            part='statistics',
+            id=channel.channelid
+        )
+        try:
+            response = statrequest.execute()
+        except Exception as e:
+            print(str(e))
+        channel.totalVid = response["items"][0]["statistics"]["videoCount"]
+        channel.subCount = response["items"][0]["statistics"]["subscriberCount"]
+        channel.totalview = response["items"][0]["statistics"]["viewCount"]
+        return channel
+
+
+    def getChannelVid(chnvid):
+        nextPageToken = None
+        for result in range(0,25):
+            channel = youtube.search().list(
+                part='snippet',
+                channelId=chnvid.channelid,
+                type='video',
+                videoDefinition='any',
+                maxResults=50,
+                videoType='any',
+                pageToken = nextPageToken
+            )
+            try:
+                response = channel.execute()
+                nextPageToken = response.get('nextPageToken')
+            except Exception as e:
+                print(str(e))
+
+
+            for vidId in response['items']:
+                chnvid.channelVids.append(vidId["id"]["videoId"])
+        return chnvid
