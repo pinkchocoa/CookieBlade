4eb65734e168a54037a935d38acf9df633294c5a
JunHao <junhaowg2@gmail.com>
2021-03-08 16:05:53 +0800

    Added Checking and Activate Twitter Crawler.

--- cookieBlade.py
+++ cookieBlade.py
@@ -1,24 +1,35 @@
 # import sys
 from GUIWidgets import * #contain class from GUI.py and GUIwidgets.py.
 from twitter import * #contain class: Twitter(), Tuser(), TTweet() #twitter crawler.
-#from youtube import * #contain class: youtubeVid() #youtube crawler. #issue with youtube crawler class.
+from youtube import * #contain class: youtubeVid() #youtube crawler. #issue with youtube crawler class.
 from database import * #contain class: database(), mkFolder(), UrlExtraction() #database and link validation.
 from LinkValidation import LinkValidation #this contain UrlExtraction as well().
+from UrlExtraction import UrlExtraction
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox #red underline can be ignored. for now..
 
 #persistant data
-ytURL = ""
-twURL = ""
+ytURL = "" #https://www.youtube.com/channel/UCmGSJVG3mCRXVOP4yZrU1Dw #Johnny Harris
+twURL = "" #https://twitter.com/johnnywharris #Johnny harris
 ytUser = ""
 twUser = ""
 
 #Link Validation and input correctness checks before crawling
 def startCrawl():
 
-    global ytURL, twURL
+    global ytURL, twURL, twUser
     ytURL = ytBox.textbox.text()
     twURL = twBox.textbox.text()
+    UID = UrlExtraction()
+    twUser = UID.getUniqueID(twURL)
+ 
+    linkCheck()
+    twCrawl()
+
+
+#Check Link and Entry Fields
+def linkCheck():
+    global twURL,ytURL
 
     #Check if Box is empthy
     if (ytURL == "" or twURL == ""):
@@ -64,31 +75,81 @@ def startCrawl():
             msgBox.setText("Invalid Twitter URL")
             msgBox.exec_()
         else:
-            pass #maybe continue from here?
+            return True
+
+#start twitter crawling
+#crawl and save to database
+def twCrawl():
+    
+    global twURL,twUser
+
+    data = [] #max 10 items.
+    #Pad data list with None
+    for i in range(10):
+        data.append(None)
+
+    #begin crawling and adding to list
+    twitterCrawl = TUser(twUser)
+    temp = twitterCrawl.followCount()
+    data[0] = str(temp)
+    temp = twitterCrawl.tweetCount()
+    data[1] = str(temp)
+
+    #store to database
+    store = database()
+    store.insertTableDB(twURL,data)
+
+ 
+#start youtube crawling
+#crawl and save to database
+def ytCrawl():
+    pass
+
+#show results
+def resultWindow():
+    pass
 
 
 
 
 
-#main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#mainStart
 App = StartApp()
 mainWindow = NewWindow("Cookie Crawler", 800, 600)
 mainWindow.setWindowIcon("CookieIcon.png")
 windowLogo = NewLabel(mainWindow.QWin, 230, 70, 331, 81)
 windowLogo.setText("")
 windowLogo.setImage("GUIMainLogo.PNG")
+
+#set Label
 urlLabel = NewLabel(mainWindow.QWin,60, 160, 61, 31)
 urlLabel.setText("Enter URL:")
 
-#youtube box
+#set youtube box
 ytBox = NewTextBox(mainWindow.QWin, 120, 160, 591, 31)
 ytBox.setText("Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>")
 
-#twitter box
+#set twitter box
 twBox = NewTextBox(mainWindow.QWin, 120, 200, 591, 31)
 twBox.setText("Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>")
 
-#Crawl Button
+#set Crawl Button
 searchPushButton = NewPushButton(mainWindow.QWin, 370, 250, 81, 41, startCrawl)
 searchPushButton.setText("Crawl Link!")
 
