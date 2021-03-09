a8302ed67098a1dbf1235d1233f8c16480261da4
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-03-09 15:26:25 +0800

    new window gen class

--- cookieBlade.py
+++ cookieBlade.py
@@ -1,12 +1,12 @@
 # import sys
-from GUIWidgets import * #contain class from GUI.py and GUIwidgets.py.
 from twitter import Twitter, TUser, TTweet #contain class: Twitter(), Tuser(), TTweet() #twitter crawler.
 from youtube import * #contain class: youtubeVid() #youtube crawler. #issue with youtube crawler class.
 from database import database #contain class: database()
 from LinkValidation import LinkValidation #contain class: LinkValidation
 from UrlExtraction import UrlExtraction
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMessageBox #red underline can be ignored. for now..
+from windowGen import windowGen
+from GUIWidgets import StartApp #contain class from GUI.py and GUIwidgets.py.
+
 
 #persistant data
 youtubeURL = "" #https://www.youtube.com/channel/UCmGSJVG3mCRXVOP4yZrU1Dw #Johnny Harris
@@ -118,28 +118,19 @@ def resultWindow():
 
 #main
 App = StartApp() #init
-mainWindow = NewWindow("Cookie Crawler", 800, 600)
-mainWindow.setWindowIcon("CookieIcon.png")
-windowLogo = NewLabel(mainWindow.QWin, 230, 70, 331, 81)
-windowLogo.setText("")
-windowLogo.setImage("GUIMainLogo.PNG")
 
-#set Label
-urlLabel = NewLabel(mainWindow.QWin,60, 160, 61, 31)
-urlLabel.setText("Enter URL:")
-
-#set youtube box
-ytBox = NewTextBox(mainWindow.QWin, 120, 160, 591, 31)
-ytBox.setText("Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>")
-
-#set twitter box
-twBox = NewTextBox(mainWindow.QWin, 120, 200, 591, 31)
-twBox.setText("Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>")
-
-#set Crawl Button
-searchPushButton = NewPushButton(mainWindow.QWin, 370, 250, 81, 41, startCrawl)
-searchPushButton.setText("Crawl Link!")
-
-mainWindow.QWin.show()
+wWidth = 800
+wHeight = 600
+nLabel = 1
+nText = 2
+nPush = 1
+w = windowGen("Cookie Crawler", wWidth, wHeight, nLabel, nText, nPush)
+w.setWindowIcon("CookieIcon.png")
+w.setLabel(230, 70, 331, 81, "","GUIMainLogo.PNG")
+w.setLabel(60, 160, 61, 31,"Enter URL:")
+w.setTextbox(120, 160, 591, 31,"Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>")
+w.setTextbox(120, 200, 591, 31,"Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>")
+w.setPush(370, 250, 81, 41, startCrawl,"Crawl Link!")
+w.showWindow()
 
 sys.exit(App.QApp.exec_()) #System X
