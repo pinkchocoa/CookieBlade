ec6f819e12c5817f5abaaba9949ff887a3ec97ad
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-03-09 10:25:15 +0800

    i changed it all to a class but still need to remove threads

--- spiderThreads.py
+++ spiderThreads.py
@@ -4,67 +4,76 @@ from spider import Spider
 from domain import *
 from general import *
 
-NUMBER_OF_THREADS = 8
-NUMBER_OF_RESULTS = 5
-PROJECT_NAME = 'googleNews'
-QUEUE_FILE = PROJECT_NAME + '/queue.txt'
-CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-RESULT_FILE = PROJECT_NAME + '/result.txt'
-queue = Queue() #create queue for spider threads
-alive = True
+class spiderWorker():
+    NUMBER_OF_THREADS = 1
+    NUMBER_OF_RESULTS = 3
+    PROJECT_NAME = 'googleNews'
+    QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+    CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+    RESULT_FILE = PROJECT_NAME + '/result.txt'
+    queue = Queue() #create queue for spider threads
+    alive = True
 
-class MyWorker(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self.daemon = True
-        self.start()
-    def run(self):
-        global alive
-        while alive:
-            result_links = file_to_set(RESULT_FILE)
-            if len(result_links) >= NUMBER_OF_RESULTS:
-                print("enough results")
-                alive = False
-                break
-            url = queue.get()
-            Spider.crawl_page(threading.current_thread().name, url)
-            queue.task_done()
+    def __init__(self, filterList, topic=""):
+        HOMEPAGE = 'https://news.google.com/search?q='
+        DOMAIN_NAME = get_domain_name(HOMEPAGE)
+        SUB_DOMAIN = get_sub_domain_name(HOMEPAGE)
+        print(DOMAIN_NAME)
+        for word in topic.split():
+            HOMEPAGE += word
+            HOMEPAGE += "+"
+            print (HOMEPAGE)
+        
+        Spider(self.PROJECT_NAME, HOMEPAGE, DOMAIN_NAME, SUB_DOMAIN, filterList) #create first spider
+        self.create_workers()
+        self.crawl()
+        print("end init")
 
-# Create worker threads (will die when main exits)
-def create_workers():
-    for x in range(NUMBER_OF_THREADS):
-        MyWorker()
+    class SpiderThread(threading.Thread):
+        def __init__(self, outerClass):
+            super().__init__()
+            self.outerClass = outerClass
+            self.daemon = True
+            self.start()
+        def run(self):
+            while self.outerClass.alive:
+                result_links = file_to_set(self.outerClass.RESULT_FILE)
+                if len(result_links) >= self.outerClass.NUMBER_OF_RESULTS:
+                    print("enough results")
+                    self.outerClass.alive = False
+                    break
+                url = self.outerClass.queue.get()
+                Spider.crawl_page(threading.current_thread().name, url)
+                self.outerClass.queue.task_done()
+            
 
-# Each queued link is a new job
-def create_jobs():
-    for link in file_to_set(QUEUE_FILE):
-        queue.put(link)
-    queue.join()
-    crawl()
+    # Create worker threads (will die when main exits)
+    def create_workers(self):
+        print("create_workers")
+        for x in range(self.NUMBER_OF_THREADS):
+            spiderWorker.SpiderThread(self)
 
-# Check if there are items in the queue, if so crawl them
-def crawl():
-    global alive
-    queued_links = file_to_set(QUEUE_FILE)
+    # Each queued link is a new job
+    def create_jobs(self):
+        print("create_jobs")
+        for link in file_to_set(self.QUEUE_FILE):
+            self.queue.put(link)
+        self.queue.join()
+        if self.alive:
+            crawl()
 
-    if alive and len(queued_links) > 0:
-        print(str(len(queued_links)) + ' links in the queue')
-        create_jobs()
+    # Check if there are items in the queue, if so crawl them
+    def crawl(self):
+        print("crawl")
+        queued_links = file_to_set(self.QUEUE_FILE)
 
-def createSpider(filterList, topic=""):
-    HOMEPAGE = 'https://news.google.com/search?q='
-    DOMAIN_NAME = get_domain_name(HOMEPAGE)
-    SUB_DOMAIN = get_sub_domain_name(HOMEPAGE)
-    print(DOMAIN_NAME)
-    for word in topic.split():
-        HOMEPAGE += word
-        HOMEPAGE += "+"
-        print (HOMEPAGE)
-    
-    Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME, SUB_DOMAIN, filterList) #create first spider
-    create_workers()
-    crawl()
-    
+        if self.alive and len(queued_links) > 0:
+            print(str(len(queued_links)) + ' links in the queue')
+            self.create_jobs()
+        else:
+            self.queue = Queue()
 
-createSpider(['articles'], "covid test")
+spiderWorker(['articles'], "covid test")
+print("test")
+print("test")
 print("test")
