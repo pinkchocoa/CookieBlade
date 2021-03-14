47fe52970c5ea8185a1f39c0fcae25e6223b045e
wjayjay123 <77979635+wjayjay123@users.noreply.github.com>
2021-03-15 00:39:57 +0800

    button works but only once, image missing

--- window.py
+++ window.py
@@ -3,16 +3,52 @@ from cookieBlade import startCrawl
 
 class UI:
     
-    def mainMenu():
+    def __init__(self):
+        app = StartApp()
+        mainM = self.mainMenu()
+        mainM.show()
+        sys.exit(app.QApp.exec_())
 
-        def mainToUser():
-            mainM.hide()
-            um = self.userMenu()
-            um.show()
+    def mainToUser(self):
+        userM = self.userMenu()
+        self.mainM.hide()
+        userM.show()
 
-        def mainToTopic():
-            pass
+    def mainToTopic(self):
+        topicM = self.topicMenu()
+        self.mainM.hide()
+        topicM.show()
 
+    def userCrawlClicked(self):
+        snsM = self.snsMenu()
+        self.userM.hide()
+        self.prev = "user"
+        snsM.show()
+
+    def userBackClicked(self):
+        self.userM.hide()
+        self.mainM.show()
+
+    def topicCrawlClicked(self):
+        snsM = self.snsMenu()
+        self.topicM.hide()
+        self.prev = "topic"
+        snsM.show()
+
+    def topicBackClicked(self):
+        self.topicM.hide()
+        self.mainM.show()
+
+    def snsBackClicked(self):
+        if self.prev == "user":
+            self.snsM.hide()
+            self.userM.show()
+        if self.prev == "topic":
+            self.snsM.hide()
+            self.topicM.show()
+
+    def mainMenu(self):
+        
         wWidth = 1080
         wHeight = 720
 
@@ -31,21 +67,16 @@ class UI:
         buttonX = logoX
         buttonY = logoY + buttonYSpace
 
-        mainM = windowGen("Cookie Crawler", wWidth, wHeight)
-        mainM.setWindowIcon("CookieIcon.png")
-        mainM.setLabel(logoX, logoY, logoWidth, logoHeight, "","GUIMainLogo.PNG")
-        mainM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, mainToUser,"User")
-        mainM.setPush(buttonX + buttonXSpace, buttonY, buttonWidth, buttonHeight, mainToTopic,"Topic")
-        return mainM
-
-    def userMenu():
         
-        def crawlClicked():
-            pass
+        self.mainM = windowGen("Cookie Crawler", wWidth, wHeight)
+        self.mainM.setWindowIcon("CookieIcon.png")
+        self.mainM.setLabel(logoX, logoY, logoWidth, logoHeight, "","GUIMainLogo.PNG")
+        self.mainM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, self.mainToUser,"User")
+        self.mainM.setPush(buttonX + buttonXSpace, buttonY, buttonWidth, buttonHeight, self.mainToTopic,"Topic")
+        return self.mainM
 
-        def backClicked():
-            pass
-
+    def userMenu(self):
+        
         wWidth = 1080
         wHeight = 720
         fontSize = 10
@@ -70,19 +101,19 @@ class UI:
         buttonX = textX + (buttonWidth/2)
         buttonY = textY + 150
 
-        userM = windowGen("User Crawler", wWidth, wHeight)
-        userM.setWindowIcon("CookieIcon.png")
-        userM.setLabel(logoX, logoY, logoWidth, logoHeight, "","GUIMainLogo.PNG")
-        userM.setTextbox(textX, textY, textBoxWidth, textBoxHeight, "Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>", "Ariel", fontSize)
-        userM.setTextbox(textX, textY+50, textBoxWidth, textBoxHeight, "Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>", "Ariel", fontSize)
-        userM.setLabel(labelX, labelY, labelWidth, labelHeight, "YouTube:", "", "Ariel", fontSize)
-        userM.setLabel(labelX, labelY+50, labelWidth, labelHeight, "Twitter:", "", "Ariel", fontSize)
-        userM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, crawlClicked, "Crawl!", "Ariel", fontSize)
-        userM.setPush(buttonX+500, buttonY, buttonWidth, buttonHeight, backClicked, "Back", "Ariel", fontSize)
-        userM.setLabel(labelX+55, labelY+75, labelWidth+200, labelHeight, "Note: Leave fields empty for random crawl", "", "Ariel", fontSize)
-        return userM
+        self.userM = windowGen("User Crawler", wWidth, wHeight)
+        self.userM.setWindowIcon("CookieIcon.png")
+        self.userM.setLabel(logoX, logoY, logoWidth, logoHeight, "","GUIMainLogo.PNG")
+        self.userM.setTextbox(textX, textY, textBoxWidth, textBoxHeight, "Enter Youtube Channel URL: E.g., <https://www.youtube.com/channel>", "Ariel", fontSize)
+        self.userM.setTextbox(textX, textY+50, textBoxWidth, textBoxHeight, "Enter Twitter User URL: E.g., <https://twitter.com/leehsienloong>", "Ariel", fontSize)
+        self.userM.setLabel(labelX, labelY, labelWidth, labelHeight, "YouTube:", "", "Ariel", fontSize)
+        self.userM.setLabel(labelX, labelY+50, labelWidth, labelHeight, "Twitter:", "", "Ariel", fontSize)
+        self.userM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, self.userCrawlClicked, "Crawl!", "Ariel", fontSize)
+        self.userM.setPush(buttonX+500, buttonY, buttonWidth, buttonHeight, self.userBackClicked, "Back", "Ariel", fontSize)
+        self.userM.setLabel(labelX+55, labelY+75, labelWidth+200, labelHeight, "Note: Leave fields empty for random crawl", "", "Ariel", fontSize)
+        return self.userM
 
-    def topicMenu():
+    def topicMenu(self):
 
         def crawlClicked():
             pass
@@ -114,19 +145,19 @@ class UI:
         buttonX = textX
         buttonY = textY + 150
 
-        topicM = windowGen("Topic Crawler", wWidth, wHeight)
-        topicM.setWindowIcon("CookieIcon.png")
-        topicM.setLabel(logoX, logoY, logoWidth, logoHeight, "","GUIMainLogo.PNG")
-        topicM.setTextbox(textX, textY, textBoxWidth, textBoxHeight, "Enter Topic: E.g., Java", "Ariel", fontSize)
-        topicM.setTextbox(textX, textY+50, textBoxWidth, textBoxHeight, "Enter Country: E.g., Singapore", "Ariel", fontSize)
-        topicM.setLabel(labelX, labelY, labelWidth, labelHeight, "Topic:", "", "Ariel", fontSize)
-        topicM.setLabel(labelX, labelY+50, labelWidth, labelHeight, "Country:", "", "Ariel", fontSize)
-        topicM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, crawlClicked, "Crawl!", "Ariel", fontSize)
-        topicM.setPush(buttonX+250, buttonY, buttonWidth, buttonHeight, backClicked, "Back", "Ariel", fontSize)
-        topicM.setLabel(labelX+55, labelY+75, labelWidth+200, labelHeight, "Note: Leave fields empty for random crawl", "", "Ariel", fontSize)
-        return topicM
+        self.topicM = windowGen("Topic Crawler", wWidth, wHeight)
+        self.topicM.setWindowIcon("CookieIcon.png")
+        self.topicM.setLabel(logoX, logoY, logoWidth, logoHeight, "","GUIMainLogo.PNG")
+        self.topicM.setTextbox(textX, textY, textBoxWidth, textBoxHeight, "Enter Topic: E.g., Java", "Ariel", fontSize)
+        self.topicM.setTextbox(textX, textY+50, textBoxWidth, textBoxHeight, "Enter Country: E.g., Singapore", "Ariel", fontSize)
+        self.topicM.setLabel(labelX, labelY, labelWidth, labelHeight, "Topic:", "", "Ariel", fontSize)
+        self.topicM.setLabel(labelX, labelY+50, labelWidth, labelHeight, "Country:", "", "Ariel", fontSize)
+        self.topicM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, self.topicCrawlClicked, "Crawl!", "Ariel", fontSize)
+        self.topicM.setPush(buttonX+250, buttonY, buttonWidth, buttonHeight, self.topicBackClicked, "Back", "Ariel", fontSize)
+        self.topicM.setLabel(labelX+55, labelY+75, labelWidth+200, labelHeight, "Note: Leave fields empty for random crawl", "", "Ariel", fontSize)
+        return self.topicM
 
-    def snsMenu():
+    def snsMenu(self):
         
         def backClicked():
             pass
@@ -160,18 +191,20 @@ class UI:
         totalTweets = 6
         tCreatDate = "02/02/2020"
 
-        snsM = windowGen("Crawled Data", wWidth, wHeight)
-        snsM.setWindowIcon("CookieIcon.png")
-        snsM.setLabel(logoX, logoY, logoWidth, logoHeight, "","YouTubeLogo.PNG")
-        snsM.setLabel(logoX, logoY+120, logoWidth, logoHeight, "","TwitterLogo.PNG")
-        snsM.setLabel(labelX-12, labelY-10, labelWidth, labelHeight, "Sub Count: " + str(subCount), "", "Ariel", fontSize)
-        snsM.setLabel(labelX+8, labelY+15, labelWidth, labelHeight, "Total View Count: " + str(viewCount), "", "Ariel", fontSize)
-        snsM.setLabel(labelX+10, labelY+40, labelWidth, labelHeight, "Total Video Count: " + str(videoCount), "", "Ariel", fontSize)
-        snsM.setLabel(labelX+19, labelY+65, labelWidth, labelHeight, "Created At: " + str(ytCreateDate), "", "Ariel", fontSize)
-        snsM.setLabel(labelX, labelY+110, labelWidth, labelHeight, "Follower Count: " + str(followerCount), "", "Ariel", fontSize)
-        snsM.setLabel(labelX+10, labelY+135, labelWidth, labelHeight, "Total Tweets Liked: " + str(tweetsLiked), "", "Ariel", fontSize)
-        snsM.setLabel(labelX-5, labelY+160, labelWidth, labelHeight, "Total Tweets: " + str(totalTweets), "", "Ariel", fontSize)
-        snsM.setLabel(labelX+18, labelY+185, labelWidth, labelHeight, "Created At: " + str(tCreatDate), "", "Ariel", fontSize)
-        snsM.setLabel(0, labelY+200, labelWidth+1005, labelHeight, "____________________________________________________________________________", "", "Ariel", 2*fontSize)
-        snsM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, backClicked, "Back", "Ariel", fontSize)
-        return snsM
+        self.snsM = windowGen("Crawled Data", wWidth, wHeight)
+        self.snsM.setWindowIcon("CookieIcon.png")
+        self.snsM.setLabel(logoX, logoY, logoWidth, logoHeight, "","YouTubeLogo.PNG")
+        self.snsM.setLabel(logoX, logoY+120, logoWidth, logoHeight, "","TwitterLogo.PNG")
+        self.snsM.setLabel(labelX-12, labelY-10, labelWidth, labelHeight, "Sub Count: " + str(subCount), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX+8, labelY+15, labelWidth, labelHeight, "Total View Count: " + str(viewCount), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX+10, labelY+40, labelWidth, labelHeight, "Total Video Count: " + str(videoCount), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX+19, labelY+65, labelWidth, labelHeight, "Created At: " + str(ytCreateDate), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX, labelY+110, labelWidth, labelHeight, "Follower Count: " + str(followerCount), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX+10, labelY+135, labelWidth, labelHeight, "Total Tweets Liked: " + str(tweetsLiked), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX-5, labelY+160, labelWidth, labelHeight, "Total Tweets: " + str(totalTweets), "", "Ariel", fontSize)
+        self.snsM.setLabel(labelX+18, labelY+185, labelWidth, labelHeight, "Created At: " + str(tCreatDate), "", "Ariel", fontSize)
+        self.snsM.setLabel(0, labelY+200, labelWidth+1005, labelHeight, "____________________________________________________________________________", "", "Ariel", 2*fontSize)
+        self.snsM.setPush(buttonX, buttonY, buttonWidth, buttonHeight, self.snsBackClicked, "Back", "Ariel", fontSize)
+        return self.snsM
+    
+    
