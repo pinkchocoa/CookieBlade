8adb31d03b546a3fd1b92b27a3aec212b48f598d
JunHao <junhaowg2@gmail.com>
2021-03-18 16:31:09 +0800

    added doxygen and rename twiiterDB

--- twitterDB.py
+++ twitterDB.py
@@ -1,7 +1,25 @@
+## @file twitterDB.py
+#
+# @brief this file contains database access methods for twitter.
+#
+# @section libraries_main Libraries/Modules
+# - database(local)
+#   - access to database class
+# - itertools
+#   - to flatten 2d list.
+
+
+#imports
 from database import database
 import itertools
 
+## Documentation for setTwitterGraphDB Method
+# This method store data retive from crawler into the database.
 def setTwitterGraphDB(tlink,*argument):
+    """! save data from twitter crawl to database.
+    @param tlink; twitter url or username
+    @param *argument; rtData,favData,dateData
+    """
     db = database('twitter')
     uid = db.getUniqueID(tlink)
     db.createTable(uid,"Date","Rt","Fav")
@@ -17,7 +35,13 @@ def setTwitterGraphDB(tlink,*argument):
         data.append(favData[x])
         db.insertTable(data,uid,'Date','Rt','Fav')
 
+## Documentation for getTwitterGraphDB Method
+# This method retrive twitter data from database.
 def getTwitterGraphDB(tlink):
+    """! retrive twitter related data from database.
+    @param tlink; twitter URL or username
+    @return rtData,favData,dateData;
+    """
     db = database('twitter')
     uid = db.getUniqueID(tlink)
     dateData = db.getTableData(uid,'Date')
@@ -28,12 +52,3 @@ def getTwitterGraphDB(tlink):
     favData = db.getTableData(uid,'Fav')
     favData = list(itertools.chain(*favData))
     return rtData,favData,dateData
-
-
-#main
-tlink = 'https://twitter.com/BarackObama'
-rtData,favData,dateData = getTwitterGraphDB(tlink)
-print(rtData)
-print(favData)
-print(dateData)
-
