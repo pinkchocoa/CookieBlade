5d53de4351dcd0bfdf24fb731fc2b17299d4a284
JunHao <junhaowg2@gmail.com>
2021-03-18 17:23:30 +0800

    Updated setTwitterGraphs

--- window.py
+++ window.py
@@ -14,6 +14,7 @@
 from GUIWidgets import *
 from windowGen import windowGen
 import twitterGraph
+import twitterDB
 
 ## Documentation for window Class
 # The window class intialize the different menus and their corresponding widgets
@@ -114,18 +115,17 @@ class window(object):
         self.stackedWidget.addWidget(self.topicM.window.page)
         
     def setTwitterGraphs(self, window):
-        a = ["a",1,42,13,64]
-        b = ["b",12,2,33,14]
-        c = ["c",15,23,31,14]
-        d = ["d",11,12,32,42]
-        e = ["e",19,24,35,42]
-        data = [a,b,c,d]
-        cat = ["jan", "feb", "mar", "apr"]
+        # a = ["a",1,42,13,64]
+        # b = ["b",12,2,33,14]
+        # c = ["c",15,23,31,14]
+        # d = ["d",11,12,32,42]
+        # e = ["e",19,24,35,42]
+        # data = [a,b,c,d]
+        # cat = ["jan", "feb", "mar", "apr"]
 
-        #data,cat = twitterGraph.twitterGraph(self.numberOfTweets, self.tlink)
-        print(data)
-        print(cat)
-        window.setBarChart(data, cat, 100, 100, 500, "User's Fav and RT Count")
+        twitterGraph.twitterGraph(self.numberOfTweets, self.tlink)
+        rtData,favData,dateData = twitterDB.getTwitterGraphDB(self.tlink)
+        window.setBarChart(rtData, favData, 100, 100, 500, "User's Fav and RT Count")
     
     def setYoutubeGraphs(self, window):
         pass
