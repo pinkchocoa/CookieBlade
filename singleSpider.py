5299269ad793414e3b27ea8e0a09f38fe7780296
jodie <46685749+pinkchocoa@users.noreply.github.com>
2021-03-21 23:11:58 +0800

    Update singleSpider.py

--- singleSpider.py
+++ singleSpider.py
@@ -1,3 +1,20 @@
+## @file singleSpider.py
+#
+# @brief this file contains the spiderWorker class
+#
+# @author Jodie
+#
+# @section libraries_main Libraries/Modules
+# - queue standard library
+#   - access to Queue data structure
+# - spider (local)
+#   - access to spider for crawling sites
+# - domain (local)
+#   - access to functions to extract domain names
+# - general (local)
+#   - access to generic file i/o 
+
+# Import
 from queue import Queue
 from spider import Spider
 from domain import *
@@ -6,9 +23,14 @@ from general import *
 PROJECT_NAME = 'googleNews'
 RESULT_FILE = 'result.txt'
 
+## Documentation for a spiderWorker Class
+# spiderWorker is a worker that crawls web pages
 class spiderWorker:
-    alive = True
     def __init__(self, filterList, topic=""):
+        """! spiderWorker class initializer
+        @param filterList filters for websites
+        @param topic topic to crawl for
+        """
         self.queue = Queue() #create queue for spider threads
         HOMEPAGE = 'https://news.google.com/search?q='
         DOMAIN_NAME = get_domain_name(HOMEPAGE)
@@ -23,27 +45,36 @@ class spiderWorker:
         #print("end init")
 
     def create_workers(self):
+        """! starts the crawling process
+        """
         self.create_jobs()
         self.work()
 
     # Each queued link is a new job
     def create_jobs(self):
+        """! creates jobs for the worker by lining them up in a queue
+        """
         #print("create_jobs")
         for link in self.spidey.queue:
             self.queue.put(link)
         pass
 
     def work(self):
+        """! crawl site
+        """
         #print("work")
         url = self.queue.get()
         self.spidey.crawl_page("1", url)
 
 def spidey(filterList="", topic="", numResults=3):
-    print("crawling")
-
+    """! spidey creates a spider for you
+    @param filterList filters for websites
+    @param topic topic to crawl for
+    @param numResutlts number of links to get
+    @return set of links
+    """
     topic = topic.replace("#", "")
     topic = topic.encode('ascii', 'ignore').decode()
-    print("topic: ", topic)
 
     x = spiderWorker(filterList,topic)
     while len(x.spidey.result) < numResults:
@@ -54,8 +85,3 @@ def spidey(filterList="", topic="", numResults=3):
     set_to_file(x.spidey.result, RESULT_FILE)
     
     return x.spidey.result
-
-#print(spidey(['articles'],"covid test", 3))
-#print("again")
-#print(spidey(['articles'],"SeninleyizErdoğan", 3))
-#print("test")
